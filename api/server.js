0770529c9e6151f73cceb6412bd391a21591d4a7
sahar <sahar.mohsni@gomycode.co>
2022-01-15 18:59:15 +0100

    add new route to get the tags list from products list

--- api/server.js
+++ api/server.js
@@ -1,10 +1,41 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+var tags = [];
+
+const getAllTags = () => {
+	const db = router.db.getState();
+	var items = db.items;
+ 
+	items.forEach((element) => {
+		element.tags.forEach((tag) => {
+			!tags.includes(tag) && tags.push(tag);
+		});
+	});
+};
+
+(function() {
+	getAllTags();
+})();
+
+server.get('/total-items-count', (req, res, next) => {
+	const db = router.db.getState();
+	var itemsCount = db.items.length;
+	res.jsonp(itemsCount);
+});
+
+server.get('/tags', (req, res) => {
+	res.jsonp(tags);
+});
+
+server.use(router);
+
 server.listen(4000, () => {
-  console.log('JSON Server is running')
-})
+	console.log('JSON Server is running');
+});

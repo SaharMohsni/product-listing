44709fbdb82e9a6dc8c3289e0f53d50348f98f0f
sahar <sahar.mohsni@gomycode.co>
2022-01-16 17:06:33 +0100

    css fixes

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -1,30 +1,39 @@
 import React, { useEffect } from 'react';
-import { Skeleton } from 'antd';
+import { Skeleton, Empty } from 'antd';
 import { useSelector } from 'react-redux';
 
 import './basket.css';
 import BasketItem from './basketItem/BasketItem';
 import * as skeleton from '../../utils/loading.skeleton.helper';
 import { selectBasket, selectLoading } from '../../features/selectors/products.selectors';
+import { isEmpty } from 'lodash';
 const Basket = () => {
 	const basketProducts = useSelector(selectBasket);
 	const loading = useSelector(selectLoading);
 
 	return (
 		<Skeleton avatar={{ shape: 'square' }} {...skeleton.shapeSquareBoxSkeleton(loading.fetchingProductByPage)}>
-			<div className="basket section-block ">
-				<div className="basket__products">
-					{basketProducts.productsList.map((item) => {
-						return <BasketItem item={item} key={Math.random()} />;
-					})}
-				</div>
-				<div className="basket__total-price-container global-flex-h-end-v-center">
-					<div className="basket__total-price-container__total-price product-price global-flex-h-center-v-center">
-						<span>$</span>
-						<span>{basketProducts.totalPrice}</span>
+			
+				<div className="basket section-block ">
+				{isEmpty(basketProducts.productsList) ? (
+				<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No products in your basket "/>
+			) : (
+					<>
+					<div className="basket__products">
+						{basketProducts.productsList.map((item) => {
+							return <BasketItem item={item} key={Math.random()} />;
+						})}
 					</div>
+					<div className="basket__total-price-container global-flex-h-end-v-center">
+						<div className="basket__total-price-container__total-price product-price global-flex-h-center-v-center">
+							<span>$</span>
+							<span>{basketProducts.totalPrice}</span>
+						</div>
+					</div>
+					</>
+				)}
 				</div>
-			</div>
+			
 		</Skeleton>
 	);
 };

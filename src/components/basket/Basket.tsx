ca0b90a1417f22249a86125f5b3d07641dc13651
sahar <sahar.mohsni@gomycode.co>
2022-01-18 09:48:28 +0100

    css fixes

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -16,31 +16,31 @@ const Basket = () => {
 	const loading = useSelector(selectLoading);
 
 	return (
-		<Skeleton avatar={{ shape: 'square' }} {...skeleton.shapeSquareBoxSkeleton(loading.fetchingProductByPage)}>
-			
-				<div className="basket section-block ">
+		<div className={`${isEmpty(basketProducts.productsList) && 'empty-section'} basket section-block`}>
+			<Skeleton avatar={{ shape: 'square' }} {...skeleton.shapeSquareBoxSkeleton(loading.fetchingProductByPage)}>
 				{isEmpty(basketProducts.productsList) ? (
-				<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No products in your basket "/>
-			) : (
-					<>
-					<div className="basket__products global-scroll">
-						{basketProducts.productsList.map((item) => {
-							return <BasketItem item={item} key={Math.random()} />;
-						})}
-					</div>
-					<Skeleton {...skeleton.fullRowItemSkeleton(loading.incrementQuantity || loading.decrementQuantity)}>
-					<div className="basket__total-price-container global-flex-h-end-v-center">
-						<div className="basket__total-price-container__total-price product-price global-flex-h-center-v-center">
-							<span>$</span>
-							<span>{basketProducts.totalPrice}</span>
+					<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No products in your basket " />
+				) : (
+					<div>
+						<div className="basket__products global-scroll">
+							{basketProducts.productsList.map((item) => {
+								return <BasketItem item={item} key={Math.random()} />;
+							})}
 						</div>
+						<Skeleton
+							{...skeleton.fullRowItemSkeleton(loading.incrementQuantity || loading.decrementQuantity)}
+						>
+							<div className="basket__total-price-container global-flex-h-end-v-center">
+								<div className="basket__total-price-container__total-price product-price global-flex-h-center-v-center">
+									<span>$</span>
+									<span>{basketProducts.totalPrice}</span>
+								</div>
+							</div>
+						</Skeleton>
 					</div>
-					</Skeleton>
-					</>
 				)}
-				</div>
-			
-		</Skeleton>
+			</Skeleton>
+		</div>
 	);
 };
 

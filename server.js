e55e8ceab895ce75bc13d76c040ea00081e2bf71
sahar <sahar.mohsni@gomycode.co>
2022-01-19 12:29:40 +0100

    setup serverjs

---
+++ server.js
@@ -0,0 +1,79 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db/db.json');
+const middlewares = jsonServer.defaults();
+
+
+
+const express = require('express');
+const path = require('path');
+
+
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+const db = router.db.getState();
+const items = db.items;
+
+var itemsTags = [];
+var itemsTypes = [];
+
+const getAllTags = () => {
+	items.forEach((element) => {
+		element.tags.forEach((tag) => {
+			!itemsTags.includes(tag) && itemsTags.push(tag);
+		});
+	});
+};
+
+const getAllTypes = () => {
+	items.forEach((element) => {
+		!itemsTypes.includes(element.itemType) && itemsTypes.push(element.itemType);
+	});
+};
+
+(function() {
+	getAllTags();
+	getAllTypes();
+})();
+
+
+
+/**** *************** Using Express ******************* ****/
+server.use(express.static(path.join(__dirname, 'build')));
+ 
+
+server.get('/market', function (req, res) {
+  res.sendFile(path.join(__dirname,   'build',  'index.html'));
+});
+
+
+/*****************************************/
+
+/**** Apis added that we don't need to fetch all products : huge number of products ****/
+// Products total count
+server.get('/total-items-count', (req, res, next) => {
+	var itemsCount = db.items.length;
+	res.jsonp(itemsCount);
+});
+
+// Products types count
+server.get('/products-types', (req, res) => {
+	res.jsonp(itemsTypes);
+});
+
+// Products tags count
+server.get('/tags', (req, res) => {
+	res.jsonp(itemsTags);
+});
+
+/**********************************************/
+
+
+server.use(router);
+
+server.listen(4000, () => {
+	console.log('JSON Server is running');
+});

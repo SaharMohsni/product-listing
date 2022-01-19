717d9c4917f18338534ae26668f7a30e45165c1c
sahar <sahar.mohsni@gomycode.co>
2022-01-19 13:35:16 +0100

    add navigate market

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from './app/Layout/Layout';
 import MarketPage from './pages/MarketPage';
+import { useNavigate } from 'react-router-dom';
 import './app/global-styles.css';
 function App() {
+	let navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('../market', { replace: true });
+	}, []);
+
 	return (
 		<div className="App">
 			<Layout>

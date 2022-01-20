6aee3640c48fc37382ecb39891ea2481de3ebc7a
sahar <sahar.mohsni@gomycode.co>
2022-01-20 20:33:52 +0100

    add empty case to fetch by page service

--- src/utils/service.helper.ts
+++ src/utils/service.helper.ts
@@ -7,30 +7,36 @@ import { isEmpty } from 'lodash';
 export const addSortToUrl = (variable: string | undefined, sortType: string | undefined) => {
 	if (!isEmpty(variable) && !isEmpty(sortType)) {
 		return `&_sort=${variable}&_order=${sortType}`;
-	} else return '';
+	}
+	return '';
 };
 
 export const addFilterToUrl = (variable: any | undefined) => {
+	console.log('🚀 ~ file: service.helper.ts ~ line 15 ~ addFilterToUrl ~ variable', variable);
 	let str = [];
-	if (variable) {
+	if (variable && !isEmpty(variable)) {
 		for (var el in variable.data) {
 			str.push(variable.key + '=' + variable.data[el]);
 		}
+		return str.join('&');
 	}
-	return str.join('&');
+	return '';
 };
 
 export const addFilterByArrayListToUrl = (variable: any | undefined) => {
+	console.log('🚀 ~ file: service.helper.ts ~ line 26 ~ addFilterByArrayListToUrl ~ variable', variable);
 	let str = [];
-	if (variable) {
+	if (variable && !isEmpty(variable)) {
 		for (var el in variable.data) {
 			str.push(variable.key + '_like=' + variable.data[el]);
 		}
+		return str.join('&');
 	}
-	return str.join('&');
+	return '';
 };
 
 export const addFilterByStringToUrl = (variable: any | undefined) => {
+	console.log('🚀 ~ file: service.helper.ts ~ line 38 ~ addFilterByStringToUrl ~ variable', variable);
 	if (!isEmpty(variable)) {
 		return `itemType=${variable}`;
 	} else return '';

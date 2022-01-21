7a1c3c3c456d47f5e446d89a1b55c33bed3fd916
sahar <sahar.mohsni@gomycode.co>
2022-01-21 20:32:24 +0100

    add redirect that we can fetch the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Layout from './app/Layout/Layout';
 import MarketPage from './pages/MarketPage';
 import './app/global-styles.css';
 
 function App() {
+	let navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('../market', { replace: true });
+	}, []);
+
 	return (
 		<div className="App">
 			<Layout>
